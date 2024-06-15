0298e8444f634d30c4ef594c991c65b95053ff28
lucasfariafr <lucas.faria.fr@gmail.com>
2024-06-15 23:44:48 +0200

    display articles in list

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    publication_date = models.DateTimeField(auto_now_add=True)
+    reading_time = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
+    
--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+def displayArticle(request):
+    articles = Article.objects.all().order_by('-publication_date')
+    return render(request, 'blog.html', {'articles': articles})
